2a2acdb68f660097262eacc6154a43c82fe40697
ryichang <ryichang@gmail.com>
2015-10-13 01:14:24 -0700

    Added README

--- base.js
+++ base.js
@@ -3,33 +3,41 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   // code in here
+  //set up variable for count
+  var count = 0;
 
- 
 
   $('#new-post-form').on('click', function(e) { 
+  	//prevent page refresh
   	e.preventDefault(); 
-
+  	//check length of post
   	if ($('#post-content').val().trim().length > 0) {
   	var postContent = $('#post-content').val();
   	var newPost = '<li class="list-group-item">' + postContent + '<span class="glyphicon glyphicon-remove pull-right">'+ '</li>';
+  	//add new posts to posts
   	$('#posts').append(newPost);
+  	localStorage.setItem('newPost', (this.newPost));
   	$('#post-content').val('');
   	console.log('#posts');
-
+  	// each post addes one count 
+  	count +=1; 
+  	console.log(count);
   }
+
+ 
 
 // tooltip function
   $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
-
+//adding Glyphicon click to remove lists 
   $(function(){
-  	$('li').on('click', function() {
-  		$(this).animate({
+  	$('.glyphicon-remove').on('click', function() {
+  		$('li').animate({
   			opacity: 0.0,
   			paddingLeft: '+=80'
   		}, 500, function() {
-  			$(this).remove();
+  			$('li').remove();
   		});
   	});
   });
@@ -38,50 +46,7 @@ $(document).ready(function(){
 
 
 
-
-
-
-  	//   var input = $('#inputType').val(); 
-
-  	//   if($())
-  	
-  	// $('#lists').append("<li class='list-group-item'>"+input+"</li>");
-  	// console.log(input);
-  	// $('#post').empty(); 
-
-	// $(':header').addClass('headline');
- //  	$('li:li(i)')).hide().fadeIn(1500);
- //  	$('li').on('click'), function() {
- //  		$(this).remove();
- //  	});
-  
-
-
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
